4b2ea2cd11380c720cbd816593886aa7d8abe329
rafa.hidalgo <rahife@users.noreply.github.com>
2015-11-12 18:49:17 +0700

    create-project working ok again

--- create-bank-holidays.sh
+++ create-bank-holidays.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#Create Staffing Groups:
+#WIP
 
-URL="http://localhost:5555/api/admin/holiday/"
+set -e
 
-echo "BANK HOLIDAYS: "$URL
+cd $(dirname ${BASH_SOURCE[0]})
 
-curl -X POST -H "Content-Type: application/json" -d '{"date":"2014-10-09T00:00:00.000Z"}' $URL
+URL="$1/api/admin/holiday/"
+
+START=1
+END=1
+for ((i=START; i<=END; i++))
+do
+    curl -X POST -H "Content-Type: application/json" -d '{"date":"2015-10-09T00:00:00.000Z"}' $URL
+done
 echo ""
 
--- create-env.sh
+++ create-env.sh
@@ -3,20 +3,22 @@
 #Create environment to test:
 #WIP
 #$1: How many ...
-#$2: URL > ex: http://52.17.174.169:5000
+#$2: URL > ex: http://52.17.174.169
 
 set -e
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-NUM_STAFFING_GROUPS=5
-NUM_COMPETENCIES=5
-NUM_RESOURCES=5
+NUM_STAFFING_GROUPS=4
+NUM_COMPETENCIES=4
+NUM_RESOURCES=4
 NUM_PROJECTS=5
-URL='http://52.17.174.169:5000'
+URL='http://localhost:5555'
 
 ./create-staffing-groups.sh $NUM_STAFFING_GROUPS $URL |
 xargs -I {} ./create-resources.sh $NUM_RESOURCES {} $URL |
 xargs -I {} ./create-project.sh {} $URL
 
 ./create-competencies.sh $NUM_COMPETENCIES $URL
+
+./create-bank-holidays.sh $URL
