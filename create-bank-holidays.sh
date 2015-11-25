6ee58abbf91815a81a83b1c01f551cfb41b30325
rafa.hidalgo <rahife@users.noreply.github.com>
2015-11-25 16:39:34 +0700

    Adding hardcoded desktops

--- create-bank-holidays.sh
+++ create-bank-holidays.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Create Staffing Groups:
+#Create Bank Holidays
 #WIP
 
 set -e
---
+++ create-desktops.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash 
+
+#Create Desktops
+#WIP
+
+set -e
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+URL="$1/api/admin/desktop-period/"
+
+START=1
+END=1
+for ((i=START; i<=END; i++))
+do
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2014-01-15T00:00:00.000Z","endDate":"2014-12-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2015-01-15T00:00:00.000Z","endDate":"2015-03-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2015-04-15T00:00:00.000Z","endDate":"2015-05-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2015-06-15T00:00:00.000Z","endDate":"2015-07-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2015-08-15T00:00:00.000Z","endDate":"2015-10-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2015-10-15T00:00:00.000Z","endDate":"2015-12-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2016-01-15T00:00:00.000Z","endDate":"2016-03-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+    curl -X POST -H "Content-Type: application/json" -d '{"startDate":"2016-06-15T00:00:00.000Z","endDate":"2016-12-11T00:00:00.000Z","internal":2,"external":3,"operations":2}' $URL
+done
+echo ""
+
